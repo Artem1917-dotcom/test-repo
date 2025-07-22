311c5d2d0c524f401395deff33c34d26a686057e
Artem <artemlevicev559@gmail.com>
2025-07-22 22:59:06 +0300

    Update lesson_1_task_3.py

--- lesson_1_task_3.py
+++ lesson_1_task_3.py
@@ -1,5 +1,5 @@
-first_name = input ("Artem")
+first_name = input("Введите ваше имя: ")
 
-last_name =  input ("Levichev")
+last_name = input("Введите вашу фамилию: ")
 
 print(f"Вас зовут: {last_name} {first_name}")
