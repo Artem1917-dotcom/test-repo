e70947885bb2b6bbacb2c89a7141ff88734dcbfe
Artem <artemlevicev559@gmail.com>
2025-07-22 17:10:03 +0300

    Add files via upload

---
+++ lesson_1_task_3.py
@@ -0,0 +1,5 @@
+first_name = input ("Artem")
+
+last_name =  input ("Levichev")
+
+print(f"Вас зовут: {last_name} {first_name}")
---
+++ lesson_1_task_5.py
@@ -0,0 +1,5 @@
+def print_number(num):
+    print(num, end='')
+
+for digit in [8, 8, 0, 0, 5, 5, 5, 3, 5, 3, 5]:
+    print_number(digit)
