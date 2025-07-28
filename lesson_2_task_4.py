334e922d045128d2b64dc803051dcc6db39df9d7
Artem <artemlevicev559@gmail.com>
2025-07-28 08:37:49 +0300

    Update lesson_2_task_4.py

--- lesson_2_task_4.py
+++ lesson_2_task_4.py
@@ -1,12 +1,12 @@
 def fizz_buzz (n):
     for i in range (1, n+1):
-        if i % 17 == 0:
+        if i % 15 == 0:
             print ("FizzBuzz")
-        elif i % 5 == 0:
+        elif i % 3 == 0:
             print ("Fizz")
-        elif i % 7 == 0:
+        elif i % 5 == 0:
             print ("Buzz")
         else:
             print (i)
 
-fizz_buzz (21)
+fizz_buzz (17)
