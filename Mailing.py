81b76f5fc68e077a96497a7cc351ccfafed568ab
Artem <artemlevicev559@gmail.com>
2025-08-09 15:59:41 +0300

    Add files via upload

---
+++ Mailing.py
@@ -0,0 +1,17 @@
+from Address import Address
+
+class Mailing:
+    def __init__(self, to_address: Address, from_address: Address, cost: float, track: str):
+        if not isinstance(to_address, Address):
+            raise TypeError("to_address должен быть типа Address")
+        if not isinstance(from_address, Address):
+            raise TypeError("from_address должен быть типа Address")
+        if not isinstance(cost, (int, float)):
+            raise TypeError("cost должен быть числом")
+        if not isinstance(track, str):
+            raise TypeError("track должен быть строкой")
+            
+        self.to_address = to_address
+        self.from_address = from_address
+        self.cost = cost
+        self.track = track
