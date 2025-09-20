6773c885c8331570ecb582ee5d5592c970cb4b36
Artem <artemlevicev559@gmail.com>
2025-09-20 20:31:44 +0300

    Add files via upload

---
+++ config.py
@@ -0,0 +1,7 @@
+BASE_URL = "https://ru.yougile.com/api-v2"
+API_TOKEN = "your_api_token_here"  # Заменить на реальный токен
+HEADERS = {
+    "Authorization": f"Bearer {API_TOKEN}",
+    "Content-Type": "application/json",
+    "Accept": "application/json"
+}
